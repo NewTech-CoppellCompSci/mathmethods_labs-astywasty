538df9449bba4004646e4c750e726fcb9f78f131
astywasty <89485269+astywasty@users.noreply.github.com>
2022-03-11 14:48:51 -0600

    v

--- src/labs/Lab01.java
+++ src/labs/Lab01.java
@@ -1,7 +1,7 @@
 package labs;
+import java.util.Scanner;
 
-
-
+import java.util.Scanner;
 public class Lab01 {
 
 	public static void main(String[] args) {
@@ -16,16 +16,30 @@ public class Lab01 {
 
 	
 	public static void problem01() {
-		
-		
-		
+		System.out.println("Enter a positive integer: ");
+		Scanner inKey1 = new Scanner(System.in);
+		int input1 = inKey1.nextInt();
+		System.out.println("Enter another positive integer: ");
+		Scanner inKey2 = new Scanner(System.in);
+		int input2 = inKey2.nextInt();
+		int base = input1;
+		for (int i = 1; i < input2; i++) {
+			input1 = input1 * base;
+		}
+		int res = input1;
+		System.out.println(base + "^" + input2 + " = " + res);
 	}
-	
 	
 	
 	
 	public static void problem02() {
 		
+		System.out.println("Enter a positive integer: ");
+		Scanner inKey = new Scanner(System.in);
+		int input = inKey.nextInt();
+		
+		double num2 = Math.sqrt(input);
+		System.out.println(num2 + "\n");
 		
 		
 	}
@@ -34,8 +48,16 @@ public class Lab01 {
 	
 
 	public static void problem03() {
+		System.out.println("Length of side A: ");
+		Scanner inKey1 = new Scanner(System.in);
+		int input1 = inKey1.nextInt();
+		System.out.println("Length of side B: ");
+		Scanner inKey2 = new Scanner(System.in);
+		int input2 = inKey2.nextInt();
 		
-		
+		int c2 = input1 * input1 + input2 * input2;
+		double c = Math.sqrt(c2);
+		System.out.println("Hypotenuse = " + c);
 		
 
 	}
@@ -44,8 +66,22 @@ public class Lab01 {
 	
 	
 	public static void problem04() {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;
+	
+		int input = 1;
 		
+		while (input != 0) {
+			
+			System.out.println("Enter a integer: ");
+			Scanner inKey1 = new Scanner(System.in);
+			input = inKey1.nextInt();
+			max = Math.max(max, input);
+			min = Math.min(min, input);
 		
+		}
+		System.out.println("Max = " + max);
+		System.out.println("Min = " + min);
 		
 	}
 	
--- src/labs/Lab02.java
+++ src/labs/Lab02.java
@@ -1,5 +1,5 @@
 package labs;
-
+import java.util.Scanner;
 
 
 public class Lab02 {
@@ -16,9 +16,27 @@ public class Lab02 {
 
 	
 	public static void problem01() {
+		int max = Integer.MIN_VALUE;
+		int min = Integer.MAX_VALUE;	
+		System.out.println("Enter a positive integer: ");
+		Scanner inKey1 = new Scanner(System.in);
+		int input1 = inKey1.nextInt();
+		max = Math.max(max, input1);
+		min = Math.min(min, input1);
+		System.out.println("Enter another positive integer: ");
+		Scanner inKey2 = new Scanner(System.in);
+		int input2 = inKey2.nextInt();
+		max = Math.max(max, input2);
+		min = Math.min(min, input2);	
+		int scope = max - min + 1;
 		
-		
-		
+		for (int i = 0; i < 10; i++) {
+			int num = (int) (Math.random() * scope + min);
+			System.out.print(num);
+
+			
+		}
+		System.out.print("");
 		
 	}
 	
@@ -26,8 +44,19 @@ public class Lab02 {
 	
 	
 	public static void problem02() {
+		System.out.println("Enter the radius: ");
+		Scanner inKey1 = new Scanner(System.in);
+		double rad = inKey1.nextInt();
+		System.out.println("Enter the height: ");
+		Scanner inKey2 = new Scanner(System.in);
+		int height = inKey1.nextInt();
 		
+		double pi = (Math.PI);
+		rad = rad *= rad;
+		rad = rad *= pi;
 		
+		double vol = rad *= height;
+		System.out.println(vol);
 		
 	}
 
@@ -35,7 +64,26 @@ public class Lab02 {
 	
 
 	public static void problem03() {
-
+	System.out.println("Enter x1: ");
+	Scanner inKey1 = new Scanner(System.in);
+	double x1 = inKey1.nextInt();
+	System.out.println("Enter x2z: ");
+	Scanner inKey2 = new Scanner(System.in);
+	double x2 = inKey2.nextInt();
+	System.out.println("Enter y1: ");
+	Scanner inKey3 = new Scanner(System.in);
+	double y1 = inKey3.nextInt();
+	System.out.println("Enter y2: ");
+	Scanner inKey4 = new Scanner(System.in);
+	double y2 = inKey4.nextInt();
+	double x = x2 -= x1;
+	x = x *= x;
+	double y = y2 -= y1;
+	y = y *= y;
+	double xy = x += y;
+	double d = Math.sqrt(xy);
+	System.out.println("Distance between points = " + d);
+	
 		
 		
 	}
